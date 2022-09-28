e973d40d47f0a7e0b61ef5c26dd881b1605f090a
Yakira Mason <95108666+VMasonyx@users.noreply.github.com>
2022-09-28 12:34:53 -0400

    Flix Part 2 Updates

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -25,8 +25,7 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.delegate = self
         
         // Do any additional setup after loading the view.
-        print("Hello")
-        
+                
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -41,7 +40,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 
                  self.tableView.reloadData()
                  
-                 print(dataDictionary)
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
                     // TODO: Reload your table view data
@@ -73,6 +71,16 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         return cell
     }
 
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        
+        print("Loading up the details screen")
+        
+    }
 
 }
 
